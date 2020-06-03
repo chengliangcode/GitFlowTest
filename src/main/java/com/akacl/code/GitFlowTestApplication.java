ee130ad68831c3ac8772102977b75cf9077feeaa
CL <CL@Conpany.com>
2020-06-03 12:22:23 +0800

    初始化 springboot 和 maven

---
+++ src/main/java/com/akacl/code/GitFlowTestApplication.java
@@ -0,0 +1,17 @@
+package com.akacl.code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author chengliang
+ * @date 2020/6/3 12:06
+ */
+@SpringBootApplication
+public class GitFlowTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitFlowTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/akacl/code/GitFlowTestApplicationTests.java
@@ -0,0 +1,17 @@
+package com.akacl.code;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author chengliang
+ * @date 2020/6/3 12:13
+ */
+@SpringBootTest
+public class GitFlowTestApplicationTests {
+
+    @Test
+    public void contextLoads() {
+        System.out.println("ssssss");
+    }
+}
