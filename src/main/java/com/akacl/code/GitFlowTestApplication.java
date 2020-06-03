b84ec2cf1f5c6f61d05f83bf01ad2003a65ff1cc
akacl <714552682@qq.com>
2020-06-03 15:28:34 +0800

    master 提交

--- src/main/java/com/akacl/code/GitFlowTestApplication.java
+++ src/main/java/com/akacl/code/GitFlowTestApplication.java
@@ -11,7 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitFlowTestApplication {
 
     public static void main(String[] args) {
-        // dev
+        // dev master 提交
         SpringApplication.run(GitFlowTestApplication.class, args);
     }
 
